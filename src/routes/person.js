70a5671a2415ec7db6d4c1097403ddacfad9bd69
ischdry <ichowdhury4@fordham.edu>
2020-04-04 22:32:42 -0400

    query string route added

--- src/routes/person.js
+++ src/routes/person.js
@@ -1,8 +1,20 @@
 let express = require('express'); 
 let router = express.Router(); 
 
+//QueryString => query property on a request object
+//localhost:3000/person?name=thomas&age=20 -> anything afer ? is qs
 router.get('/person', (req,res) => { 
-    res.send('you have requested a person');
-})
+    if(req.query.name) {
+        res.send(`you have requested a person ${req.query.name}`);
+    }
+    else {
+        res.send('you have requested a person');
+    }
+});
+//params property on the requst object
+router.get("/person/:name", (req, res) => {
+        res.send(`you have requested a person ${req.params.name}` );
+});
+
 
 module.exports = router; // will let us import files 
